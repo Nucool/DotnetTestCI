7b11effb9e981bd04e7684abe0136a81be115734
nucool <nop_za16@hotmail.com>
2018-02-13 19:04:30 +0700

    Update code error for test CI Build

--- DotnetTestCI/Controllers/HomeController.cs
+++ DotnetTestCI/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace DotnetTestCI.Controllers
         public ActionResult Index()
         {
             return View(); 
+            asdasdasdasd
         }
 
         public ActionResult About()
