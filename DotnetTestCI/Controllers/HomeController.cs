bb21640a339c0e670216853a67210ba3551c86a3
nucool <nop_za16@hotmail.com>
2018-02-13 19:02:52 +0700

    Update space

--- DotnetTestCI/Controllers/HomeController.cs
+++ DotnetTestCI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace DotnetTestCI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
         public ActionResult About()
