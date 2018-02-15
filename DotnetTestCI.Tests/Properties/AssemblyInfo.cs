e9c14b3ccd9b4e596d6495f11a07eaa831cab71c
nucool <nop_za16@hotmail.com>
2018-02-15 10:34:27 +0700

    Add test project

---
+++ DotnetTestCI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotnetTestCI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotnetTestCI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f22f3c0-3162-4ab4-94c1-ce7f1357414f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotnetTestCI.Tests/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DotnetTestCI.Controllers;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Security.Principal;
+using System;
+
+namespace DotnetTestCI.Tests
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("TestMethod1 : ");
+        }
+
+        [TestMethod]
+        public void Index_Get_AsksForIndexView()
+        {
+            // Arrange
+            var controller = GetHomeController();
+            // Act
+            ViewResult result = (ViewResult)controller.Index();
+
+            Console.WriteLine("viewname : " + result.ViewName);
+            // Assert
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+
+        private static HomeController GetHomeController()
+        {
+            HomeController controller = new HomeController();
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                Controller = controller,
+                RequestContext = new RequestContext(new MockHttpContext(), new RouteData())
+            };
+            return controller;
+        }
+
+        private class MockHttpContext : HttpContextBase
+        {
+            private readonly IPrincipal _user = new GenericPrincipal(
+                     new GenericIdentity("someUser"), null /* roles */);
+
+            public override IPrincipal User
+            {
+                get
+                {
+                    return _user;
+                }
+                set
+                {
+                    base.User = value;
+                }
+            }
+        }
+
+    }
+}
